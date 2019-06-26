12ec3c10e50b890071046c48fbce3da3bafe093d
Patipon Riebpradit <50250790+ShubUtum@users.noreply.github.com>
2019-06-26 17:30:09 +0200

    done eiei

--- main.c
+++ main.c
@@ -5,26 +5,10 @@
 #include <libpic30.h>
 
 #include "lcd.h"
+#include "touch.h"
+#include "servo.h"
 
 
-// start: Servo configurtaion macros
-
-// 4000= 20*10^-3 * 12.8*10^6 * 1/64
-// 1000= 20*10^-3 * 12.8*10^6 * 1/256
-#define PERIOD 1000 // timer period 20ms:
-// duty cycle = pulse width / period 
-// 0.9ms = 0 degree
-#define ZERO (0.9*PERIOD)/20
-// 1.5ms = 90 degree
-#define NINETY (1.5*PERIOD)/20
-// 2.1ms = 180 degree
-#define ONEEIGHTY (2.1*PERIOD)/20
-
-#define CH_X 0
-#define CH_Y 1
-
-
-// end: Servo configuration macros
 
 
 
@@ -54,139 +38,11 @@ OC8,OC8 pin will be set to high for ... ms every 20ms. */
 
 
 
-void setupServo(uint8_t servoNum)
-{
-    //setup Timer 2
-    CLEARBIT(T2CONbits.TON); // Disable Timer
-    CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
-    CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
-    TMR2 = 0x00; // Clear timer register
-    T2CONbits.TCKPS = 0b11; // Select 1:64 Prescaler
-    CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
-    CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
-    PR2 = PERIOD; // Set timer period 20ms:
-   
-    
-    if(servoNum == CH_X)
-    {
-        //setup OC7
-        CLEARBIT(TRISDbits.TRISD7); /* Set OC7 as output */
-        OC7R = NINETY; /* Set the initial duty cycle to 1.5 ms (90 degree) */
-        OC7RS = NINETY; /* Load OCRS: next pwm duty cycle */
-        OC7CON = 0x0006; /* Set OC7: PWM, no fault check, Timer2 */
-        SETBIT(T3CONbits.TON); /* Turn Timer 3 on */
-    }
 
-    else if(servoNum == CH_Y)
-    {
-        //setup OC8
-        CLEARBIT(TRISDbits.TRISD8); 
-        OC8R = NINETY; 
-        OC8RS = NINETY; 
-        OC8CON = 0x0006; 
-        SETBIT(T2CONbits.TON);
-    }
-    
-}
-
-// duty cycle in microsecs
-void setDutyCycle(uint8_t servoNum, uint8_t dutyCycle)
-{
-    if(servoNum == CH_X)
-    {
-        OC7RS = PERIOD - dutyCycle; // next duty cycle 
-    }
-    else if(servoNum == CH_Y)
-    {
-        OC8RS = PERIOD - dutyCycle; 
-    }
-}
-
-
-// end: Servo configuration functions
 
 // start: Screen config
 
-void initTouchScreen()
-{
-    CLEARBIT(TRISEbits.TRISE1);
-    CLEARBIT(TRISEbits.TRISE2);
-    CLEARBIT(TRISEbits.TRISE3);
-    
-    
-    SETBIT(PORTEbits.RE1);
-    SETBIT(PORTEbits.RE2);
-    CLEARBIT(PORTEbits.RE3);
-}
 
-
-void initADC(){
-
-    // 0. Disable ADC1
-    CLEARBIT(AD1CON1bits.ADON);
-
-    // 1. set input pins
-    SETBIT(TRISBbits.TRISB15); // set input. AN15
-    SETBIT(TRISBbits.TRISB9); // set input. AN9
-
-    // 2. set analog pins
-    CLEARBIT(AD1PCFGLbits.PCFG15); // set analog. X is hardwired to AN15
-    CLEARBIT(AD1PCFGLbits.PCFG9); // set analog. Y is hardwired to AN9
-
-    // 3. Configure AD1CON1
-    SETBIT(AD1CON1bits.AD12B); // set 12-bit mode
-    AD1CON1bits.FORM = 0; // set integer output
-    AD1CON1bits.SSRC = 0x7; // set automatic conversion
-
-    // 4. Configure AD1CON2
-    AD1CON2 = 0; // not using scanning sampling
-
-    // 5. Configure AD1CON3
-    CLEARBIT(AD1CON3bits.ADRC); // internal clock source
-    AD1CON3bits.SAMC = 0x1F; // sample-to-conversion clock = 31Tad
-    AD1CON3bits.ADCS = 0x2; // Tad = 3Tcy (Time cycles)
-
-    // 6. Enable ADC1
-    SETBIT(AD1CON1bits.ADON);
-}
-
-uint16_t readADC(){
-    SETBIT(AD1CON1bits.SAMP); // start to sample
-    while(!AD1CON1bits.DONE); // wait for conversion to finish
-    CLEARBIT(AD1CON1bits.DONE); // MUST HAVE! clear conversion done bit
-
-    return ADC1BUF0;
-}
-
-void setDirectionADC(int direction){
-    if (direction == 0)
-        AD1CHS0bits.CH0SA = 0x000F;
-    else
-        AD1CHS0bits.CH0SA = 0x0009;
-}
-
-void setTouchMode(int direction){
-    if (direction == 0){
-        CLEARBIT(PORTEbits.RE1);
-        SETBIT(PORTEbits.RE2);
-        SETBIT(PORTEbits.RE3);
-        AD1CHS0bits.CH0SA = 0x000F;
-        __delay_ms(10);
-    }
-    else{
-        SETBIT(PORTEbits.RE1); 
-        CLEARBIT(PORTEbits.RE2);
-        CLEARBIT(PORTEbits.RE3);
-        
-        AD1CHS0bits.CH0SA = 0x0009;
-        __delay_ms(10);
-
-    }
-
-     
-        
-    
-}
 
 // end: Screen config
 
@@ -232,6 +88,7 @@ void __attribute__((interrupt)) _T3Interrupt (void) {
         }
         
          setTouchMode(0); // read x
+         __delay_ms(10);
         
         for (j = 0; j < NUM_SAMPLES; j++){
             __delay_ms(2);
@@ -239,17 +96,19 @@ void __attribute__((interrupt)) _T3Interrupt (void) {
         }
         
         lcd_locate(0, 1);
-        lcd_printf("X: %.01f\n", sum );
+        lcd_printf("Y: %.01f\n", sum );
         sum =0;
         
         
         setTouchMode(1);
+        __delay_ms(10);
+        
         for (j = 0; j < NUM_SAMPLES; j++){
             __delay_ms(2);
             sum = readADC();
         }
         lcd_locate(0, 3);
-        lcd_printf("Y: %.01f\n", sum );
+        lcd_printf("X: %.01f\n", sum );
         sum =0; 
     }
     count++;  
